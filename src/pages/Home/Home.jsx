82f436104ee9e8eeb743e74ee6e8d8fbcced939e
Subin <70842920+bin-pro@users.noreply.github.com>
2023-09-22 14:13:13 +0900

    Chore: Github Action Test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,7 +22,8 @@ const Home = () => {
       <section className={styles.header}>
         <div className={styles.headerSubTitle}>지금 계좌 개설하면</div>
         <div className={styles.headerTitle}>
-          <span className={styles.primaryColor}>수수료 평생혜택</span> 제공
+          <span className={styles.primaryColor}>수수료 평생평생평생??</span>{" "}
+          제공
         </div>
         {/* 카드 섹션 */}
         <div className={styles.cardContainer}>
